3435b47dc7334faff3272645b4d112f262cc3614
Andrew Johnson <ajohnson6494@gmail.com>
2017-04-22 13:55:56 -0400

    Fix naming issue

--- src/AdobeSignProvider.php
+++ src/AdobeSignProvider.php
@@ -8,7 +8,7 @@ use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use Psr\Http\Message\ResponseInterface;
 
-class AdobeSign extends AbstractProvider
+class AdobeSignProvider extends AbstractProvider
 {
     protected $scope;
 
